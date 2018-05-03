e75a5610d98543551a1863f2c9ad8d3a38be180b
João Lucas <joaolucaspachecoab@gmail.com>
2018-05-03 16:06:34 -0300

    Alterações finais - Primeira versão completa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 function calcula_imc(){
-    var altura = document.getElementById("altura");
-    var peso = document.getElementById("peso");
+    var altura = document.getElementById("altura").value/100;
+    var peso = document.getElementById("peso").value;
 
     var quadrado = (altura * altura);
 
     var resultado = (peso/quadrado);
+
     if(resultado<18.5){
-        alert("Seu indice indic;a que você está abaixo do peso: " + resultado);
+        alert("Seu indice indica que você está abaixo do peso: " + resultado);
     }
     else if(resultado>=18.5 && resultado<24.9){
         alert("Seu indice indica que você está no peso ideal: " + resultado);
     }
-
     else if(resultado>=25 && resultado<29.9) {
         alert("Seu indice indica que você está acima do peso: " + resultado);
     }
     else if(resultado>=30 && resultado<39.9) {
         alert("Seu indice indica que você está com obesidade: " + resultado);
     }
-    else if (resultado>40)
+    else if (resultado>40) {
         alert("Seu indice indica que você está com obesidade grave: " + resultado);
+    }
 }
