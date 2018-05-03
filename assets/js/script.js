46b8c53680df4fd976b59f97afebf995b43c8ae5
João Lucas <joaolucaspachecoab@gmail.com>
2018-05-03 16:40:14 -0300

    Versão com modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+
 function calcula_imc(){
     var altura = document.getElementById("altura").value/100;
     var peso = document.getElementById("peso").value;
@@ -10,15 +15,15 @@ function calcula_imc(){
         alert("Seu indice indica que você está abaixo do peso: " + resultado);
     }
     else if(resultado>=18.5 && resultado<24.9){
-        alert("Seu indice indica que você está no peso ideal: " + resultado);
+        document.getElementById("texto_modal").innerText = "Seu indice indica que você está no peso ideal: " + resultado;
     }
     else if(resultado>=25 && resultado<29.9) {
-        alert("Seu indice indica que você está acima do peso: " + resultado);
+        document.getElementById("texto_modal").innerText = "Seu indice indica que você está acima do peso: " + resultado;
     }
     else if(resultado>=30 && resultado<39.9) {
-        alert("Seu indice indica que você está com obesidade: " + resultado);
+        document.getElementById("texto_modal").innerText = "Seu indice indica que você está com obesidade: " + resultado;
     }
     else if (resultado>40) {
-        alert("Seu indice indica que você está com obesidade grave: " + resultado);
+        document.getElementById("texto_modal").innerText = "Seu indice indica que você está com obesidade grave: " + resultado;
     }
 }
